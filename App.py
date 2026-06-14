07d253b536ee92592aadaf01b0158ee18605b52a
AbdulsametDogru <abdulsametdogru.25@gmail.com>
2026-06-14 13:28:36 +0300

    App.py commit1

---
+++ App.py
@@ -0,0 +1,7 @@
+import streamlit as st
+from Backend import GorevYonetimi
+import datetime
+
+# Sayfa genişliği ve başlık
+st.set_page_config(page_title="Görev Yönetimi", layout="wide", page_icon="")
+
