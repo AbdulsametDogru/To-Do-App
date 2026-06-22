2534962970b4c6af2923f80c88619f84790c32aa
AbdulsametDogru <abdulsametdogru.25@gmail.com>
2026-06-22 20:44:33 +0300

    Hata Kontrol

--- auth.py
+++ auth.py
@@ -1,6 +1,6 @@
 import hashlib
 import database
-
+import streamlit as st
 
 class Auth:
 
@@ -26,20 +26,23 @@ class Auth:
 
         return True
 
+    
+
     @staticmethod
     def giris(username, password):
 
         users = database.db_getir_kullanicilar()
 
+        st.write(users)   # GEÇİCİ
+
         sifre_hash = Auth.sifrele(password)
 
         for user in users:
-
             if (
-                user["username"] == username
+                user.get("username") == username
                 and
-                user["password"] == sifre_hash
-            ):
+                 user.get("password") == sifre_hash
+        ):
                 return True
 
         return False
